67414e602990486d37e8c679951bcba52f6d52c5
liujinan <liujinan@eheartcare.com>
2018-04-09 19:41:38 +0800

    add bottomdialog

--- buglysimple/src/main/java/com/ljn/buglysimple/MainActivity.java
+++ buglysimple/src/main/java/com/ljn/buglysimple/MainActivity.java
@@ -1,10 +1,18 @@
 package com.ljn.buglysimple;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tv_text;
     private Button btn_button;
     private Button btn_button2;
+    private RotateAnimation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +56,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, ((MyApplication)getApplication()).getBugly(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_button2:
-                Toast.makeText(MainActivity.this, MyApplication.NAME, Toast.LENGTH_SHORT).show();
+                showBottomDialog();
                 break;
         }
+    }
+
+    private void showBottomDialog() {
+        final Dialog dialog = new Dialog(this, R.style.MyBottomDialog);
+        final View view = LayoutInflater.from(this).inflate(R.layout.voiceinput, null);
+        final RelativeLayout rl_voice = (RelativeLayout) view.findViewById(R.id.rl_voice);
+        final TextView tv_voice_cancel = (TextView) view.findViewById(R.id.tv_voice_cancel);
+        final TextView tv_voice_finish = (TextView) view.findViewById(R.id.tv_voice_finish);
+        final TextView tv_hint = (TextView) view.findViewById(R.id.tv_hint);
+        Window window = dialog.getWindow();
+        window.setGravity(Gravity.BOTTOM);
+        window.setContentView(view);
+
+        WindowManager.LayoutParams lp = window.getAttributes(); // 获取对话框当前的参数值
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;//宽度占满屏幕
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setAttributes(lp);
+        dialog.show();
+
+        animation = new RotateAnimation(0.0F, 360.0F, 1, 0.5F, 1, 0.5F);
+        animation.setRepeatCount(-1);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setDuration(700L);
+
+        rl_voice.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                tv_voice_cancel.setVisibility(View.VISIBLE);
+                tv_voice_finish.setVisibility(View.VISIBLE);
+                tv_hint.setVisibility(View.INVISIBLE);
+                rl_voice.startAnimation(animation);
+                return false;
+            }
+        });
+
+        tv_voice_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        tv_voice_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
     }
 
     @Override
